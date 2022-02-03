e1ccaf6a038e6178e61237d3651848c03e64ff20
David Bootle <davidtbootle@gmail.com>
2022-02-03 17:46:37 -0500

    updated planner

--- pages/planner.bootletools.com.tsx
+++ pages/planner.bootletools.com.tsx
@@ -1,8 +1,22 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, InferGetServerSidePropsType, NextPage } from 'next'
+import Column from '../components/Column'
+import ClientResponseTimeMetric from '../components/custom-metrics/ClientResponseTimeMetric'
+import ServerResponseTimeMetric from '../components/custom-metrics/ServerResponseTimeMetric'
+import StatusMetric from '../components/custom-metrics/StatusMetric'
 import Layout from '../components/Layout'
+import MetricRow from '../components/MetricRow'
+import PageSection from '../components/PageSection'
 import PageTitleBar from '../components/PageTitleBar'
+import SectionBlock from '../components/SectionBlock'
 
-const VisualSchedulePlanner: NextPage = () => {
+type Data = {
+  online: boolean,
+  ping?: number,
+}
+
+const pingURL = 'https://planner.bootletools.com'
+
+const VisualSchedulePlanner: NextPage = (props: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   return (
     <Layout activeMenuItem={'planner.bootletools.com'}>
       <PageTitleBar
@@ -11,8 +25,49 @@ const VisualSchedulePlanner: NextPage = () => {
         websiteLink='https://planner.bootletools.com'
         subtitleIsLink={true}
       />
+      <PageSection>
+        <SectionBlock>
+          <MetricRow flush={true}>
+            <Column>
+              <StatusMetric online={props.data.online} bordered={true}/>
+            </Column>
+            <Column>
+              <ServerResponseTimeMetric responseTime={props.data.ping} url={pingURL} bordered={true}/>
+            </Column>
+            <Column>
+              <ClientResponseTimeMetric url={pingURL} bordered={true}/>
+            </Column>
+          </MetricRow>
+        </SectionBlock>
+      </PageSection>
     </Layout>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+
+  let data: Data = {
+    online: false
+  }
+
+  // fetch server status by using /ping path
+  try {
+    const responseTimeStart = new Date().getTime();
+    const res = await fetch(pingURL);
+    const responseTime = new Date().getTime() - responseTimeStart;
+    if (res.ok) {
+      data.online = true;
+      data.ping = responseTime;
+    }
+  } catch (e) {
+
+  }
+
+  return {
+    props: {
+      data
+    }
+  }
 }
 
 export default VisualSchedulePlanner
